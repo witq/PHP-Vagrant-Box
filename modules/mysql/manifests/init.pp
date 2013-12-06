3c5443210df2db714ba89fb9dc570e8d0da7ba15
Evgeny Golubev <evgeny@golubev.eu>
2013-12-06 12:25:56 +0300

    Fix mysql init

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -12,18 +12,18 @@ class mysql {
 
     exec { 'set-mysql-password':
             onlyif => "mysqladmin -uroot -p status",
-            command => "mysqladmin -uroot -proot password root",
+            command => "mysqladmin -uroot -proot password ",
             require => Service["mysql"],
     }
 
     exec { 'create-default-db':
-            unless => '/usr/bin/mysql -uroot -proot appbox',
-            command => '/usr/bin/mysql -uroot -proot -e "create database appbox;"',
-            require => [Service['mysql'], Exec['set-mysql-password']]
+            unless => '/usr/bin/mysql -uroot -p appbox',
+            command => '/usr/bin/mysql -uroot -p -e "create database appbox;"',
+            require => [Service['mysql']]
     }
 
     exec { 'grant-default-db':
-            command => '/usr/bin/mysql -uroot -proot -e "grant all on appbox.* to root@localhost;"',
+            command => '/usr/bin/mysql -uroot -p -e "grant all on appbox.* to root@localhost;"',
             require => [Service['mysql'], Exec['create-default-db']]
     }
 }
